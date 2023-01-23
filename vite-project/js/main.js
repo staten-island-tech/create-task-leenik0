c2a14f91a90728c8d10327564408d17733ea36fe
leenik0 <nicoleh.vafiadis@gmail.com>
2023-01-23 09:43:52 -0500

    g

---
+++ vite-project/js/main.js
@@ -0,0 +1,32 @@
+const URL = {
+  actors: "https://api.catalogopolis.xyz/v1/actors",
+  // companions: "https://api.catalogopolis.xyz/v1/companions",
+  doctors: "https://api.catalogopolis.xyz/v1/doctors",
+  episodes: "https://api.catalogopolis.xyz/v1/episodes",
+  seasons: "https://api.catalogopolis.xyz/v1/seasons",
+  writers: "https://api.catalogopolis.xyz/v1/writers",
+};
+const hist = [];
+const box = document.getElementById("cards");
+
+document.querySelector("#form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  show(URL.actors);
+});
+
+function the() {
+  console.log(search);
+}
+
+async function show(e) {
+  const response = await fetch(e);
+  const res = await response.json();
+  box.innerHTML = "";
+  const search = document.getElementById("input").value.toLowerCase();
+  const filtered = res.filter((el) => {
+    const namematch = el.name.toLowerCase().includes(search);
+    const docs = el.incarnation.toLowerCase().includes(search);
+    const ep = e.title.toLowerCase().includes(search);
+  });
+  console.log(filtered);
+}
